7076f8375202acf9d5e40e71eae8e10c47dd9ace
jiggy <george_hallam@hotmail.com>
2017-04-27 23:37:52 +0100

    Created basic chat sever

--- src/Chat.php
+++ src/Chat.php
@@ -1,45 +1,109 @@
 <?php
+
 namespace Messaging\sockets;
-use Ratchet\MessageComponentInterface;
+
 use Ratchet\ConnectionInterface;
+use Ratchet\Wamp\Topic;
+use Ratchet\Wamp\WampServerInterface;
 
-class Chat implements MessageComponentInterface {
-    protected $clients;
+class Chat implements WampServerInterface
+{
 
-    public function __construct() {
-        $this->clients = new \SplObjectStorage;
+    /**
+     * A lookup of all the topics clients have subscribed to
+     */
+    protected $subscribedTopics = array();
+
+    public function onSubscribe(ConnectionInterface $conn, $topic)
+    {
+        $this->subscribedTopics[$topic->getId()] = $topic;
+        echo "Someone has subscribed" . PHP_EOL;
     }
 
-    public function onOpen(ConnectionInterface $conn) {
-        // Store the new connection to send messages to later
-        $this->clients->attach($conn);
+    /**
+     * @param string JSON'ified string we'll receive from ZeroMQ
+     */
+    public function onInstantMessage($entry)
+    {
+        $entryData = json_decode($entry, true);
 
-        echo "New connection! ({$conn->resourceId})\n";
-    }
+        echo "Someone send a message" . PHP_EOL;
 
-    public function onMessage(ConnectionInterface $from, $msg) {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
-            }
+        // If the lookup topic object isn't set there is no one to publish to
+        if (!array_key_exists($entryData['server'], $this->subscribedTopics)) {
+            return;
         }
+
+        $topic = $this->subscribedTopics[$entryData['server']];
+
+        // re-send the data to all the clients subscribed to that category
+        $topic->broadcast($entryData);
     }
 
-    public function onClose(ConnectionInterface $conn) {
-        // The connection is closed, remove it, as we can no longer send it messages
-        $this->clients->detach($conn);
-
-        echo "Connection {$conn->resourceId} has disconnected\n";
+    /**
+     * When a new connection is opened it will be passed to this method
+     * @param  ConnectionInterface $conn The socket/connection that just connected to your application
+     * @throws \Exception
+     */
+    function onOpen(ConnectionInterface $conn)
+    {
+        // TODO: Implement onOpen() method.
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo "An error has occurred: {$e->getMessage()}\n";
+    /**
+     * This is called before or after a socket is closed (depends on how it's closed).  SendMessage to $conn will not result in an error if it has already been closed.
+     * @param  ConnectionInterface $conn The socket/connection that is closing/closed
+     * @throws \Exception
+     */
+    function onClose(ConnectionInterface $conn)
+    {
+        // TODO: Implement onClose() method.
+    }
 
-        $conn->close();
+    /**
+     * If there is an error with one of the sockets, or somewhere in the application where an Exception is thrown,
+     * the Exception is sent back down the stack, handled by the Server and bubbled back up the application through this method
+     * @param  ConnectionInterface $conn
+     * @param  \Exception $e
+     * @throws \Exception
+     */
+    function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        // TODO: Implement onError() method.
+    }
+
+    /**
+     * An RPC call has been received
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string $id The unique ID of the RPC, required to respond to
+     * @param string|Topic $topic The topic to execute the call against
+     * @param array $params Call parameters received from the client
+     */
+    function onCall(ConnectionInterface $conn, $id, $topic, array $params)
+    {
+        // TODO: Implement onCall() method.
+    }
+
+    /**
+     * A request to unsubscribe from a topic has been made
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string|Topic $topic The topic to unsubscribe from
+     */
+    function onUnSubscribe(ConnectionInterface $conn, $topic)
+    {
+        // TODO: Implement onUnSubscribe() method.
+    }
+
+    /**
+     * A client is attempting to publish content to a subscribed connections on a URI
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string|Topic $topic The topic the user has attempted to publish to
+     * @param string $event Payload of the publish
+     * @param array $exclude A list of session IDs the message should be excluded from (blacklist)
+     * @param array $eligible A list of session Ids the message should be send to (whitelist)
+     */
+    function onPublish(ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible)
+    {
+        // TODO: Implement onPublish() method.
     }
 }
